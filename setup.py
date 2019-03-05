200d1f1e4559901d2626ec6ec0dbd39b190ff2ab
Joshua Milas <Josh.Milas@gmail.com>
2019-03-05 00:19:23 -0500

    Updated the requirements and lock

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setuptools.setup(
     version="0.1",
     packages=['monolith'],
     scripts=['scripts/monolith'],
-    install_requires=['requests>=2.18', 'beautifulsoup4>=4.6.0'],
+    install_requires=['requests>=2.21'],
 )
